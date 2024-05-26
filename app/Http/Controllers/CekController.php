affb2a88ae06fec40038a871eb92eb649631f2e3
Feyfa <fisikamodern00@gmail.com>
2024-05-26 16:21:47 +0700

    first commit

---
+++ app/Http/Controllers/CekController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CekController extends Controller
+{
+    public function index()
+    {
+        return '<h1>Cek2</h1>';
+    }
+}
